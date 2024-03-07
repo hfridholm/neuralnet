a4fcbf2d3ed0bd11307b20c5b064b7c72ee0a9ad
Hampus Fridholm <hampus.fridholm@abbgymnasiet.se>
2024-03-07 11:54:16 +0100

    Created the file structure of this new version of neuralnet. Added the STB Image files and created a makefile.

---
+++ source/master.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+int main(int argc, char* argv[])
+{
+  printf("Hello World!\n");
+
+  return 0;
+}
